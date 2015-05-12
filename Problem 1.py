f9e0ce69527e5728e1444c782554173815644991
IceJackal <jack098y@gmail.com>
2015-05-12 10:31:59 +0100

    Create Problem 1.py
    Solution to problem 1

---
+++ Problem 1.py
@@ -0,0 +1,10 @@
+numbers = 0
+for x in range(1, 1000):
+    if x % 3 == 0:
+        numbers = numbers + x
+    elif x % 5 == 0:
+        numbers = numbers + x
+
+            
+        
+print numbers
